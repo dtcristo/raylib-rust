f709b2c5429e0cb7af3b2336792e89f9db6988d7
David Cristofaro <david@dtcristo.com>
2018-08-10 20:49:57 +1000

    Add colors

--- raylib/src/lib.rs
+++ raylib/src/lib.rs
@@ -1,9 +1,31 @@
 extern crate raylib_sys as ffi;
 
 pub mod audio;
+/// Custom raylib color palette for amazing visuals
+pub mod colors;
 pub mod core;
 pub mod models;
 pub mod shaders;
 pub mod shapes;
 pub mod text;
 pub mod textures;
+
+/// Color type, RGBA (32bit)
+#[derive(Debug, Copy, Clone)]
+pub struct Color {
+    pub r: u8,
+    pub g: u8,
+    pub b: u8,
+    pub a: u8,
+}
+
+impl Color {
+    fn into_ffi(self) -> ffi::Color {
+        ffi::Color {
+            r: self.r,
+            g: self.g,
+            b: self.b,
+            a: self.a,
+        }
+    }
+}
